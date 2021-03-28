4e52e224077b461e47c2e143caf3430d5f125d5b
Parakoopa <parakoopa@live.de>
2021-03-28 19:20:06 +0200

    Revert "Force enable the fun"
    This reverts commit 7fba7374076361bcf3e04940e2129c74deef2600.

--- skytemple_randomizer/randomizer/special/fun.py
+++ skytemple_randomizer/randomizer/special/fun.py
@@ -138,7 +138,6 @@ FunPortraitLike = Union[CustomFunPortrait, FunPortrait]
 
 
 def is_fun_allowed():
-    return True  # force enable fun
     if 'SKYTEMPLE_FUN' in os.environ:
         return bool(int(os.environ['SKYTEMPLE_FUN']))
     now = datetime.now()

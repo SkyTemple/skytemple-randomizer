de09cfcc29e0311eabb4785c865b19fb7d7a39b3
Marco Köpcke <marco.koepcke@tudock.de>
2021-10-02 22:11:35 +0200

    Reduce base amount of enemy density

--- skytemple_randomizer/randomizer/dungeon.py
+++ skytemple_randomizer/randomizer/dungeon.py
@@ -193,7 +193,7 @@ class DungeonRandomizer(AbstractRandomizer):
             music_id=randrange(1, 118),
             weather=self._randomize_weather(original_layout, dungeon_id),
             floor_connectivity=randrange(5, 51),
-            initial_enemy_density=randrange(1, 14),
+            initial_enemy_density=randrange(1, 7),
             kecleon_shop_chance=randrange(0, 101),
             monster_house_chance=
                 randrange(0, self.config['dungeons']['max_mh_chance'].value + 1) if allow_monster_houses else 0,

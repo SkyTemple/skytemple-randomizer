57b1ebfd7e7dedad70efadb87fae8d5fce794b16
Marco Köpcke <marco.koepcke@tudock.de>
2021-12-15 18:11:59 +0100

    Kao changes

--- skytemple_randomizer/randomizer/util/util.py
+++ skytemple_randomizer/randomizer/util/util.py
@@ -18,12 +18,14 @@ from enum import Enum, auto
 from random import sample, choice
 from typing import List, Dict, Union, Tuple, Iterable
 
+from ndspy.rom import NintendoDSRom
+
 from skytemple_files.common.ppmdu_config.data import Pmd2Data, Pmd2Language
 from skytemple_files.common.types.file_types import FileType
 from skytemple_files.common.util import get_files_from_rom_with_extension
 from skytemple_files.data.md.model import NUM_ENTITIES, PokeType
 from skytemple_files.data.str.model import Str
-from skytemple_files.graphics.kao.model import SUBENTRIES, NintendoDSRom
+from skytemple_files.graphics.kao import SUBENTRIES
 from skytemple_files.graphics.kao.protocol import KaoProtocol
 from skytemple_randomizer.config import RandomizerConfig
 

f76cad84eed9de9648e59739f15746d1b124429b
Marco Köpcke <marco.koepcke@tudock.de>
2022-05-17 15:53:45 +0200

    Don't download components from Git for now

--- installer/install-skytemple-components-from-git.sh
+++ installer/install-skytemple-components-from-git.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+# DISABLED until 1.4.
+exit
 pip3 uninstall -y explorerscript && pip3 install git+https://github.com/SkyTemple/explorerscript.git
 pip3 uninstall -y skytemple-files && pip3 install git+https://github.com/SkyTemple/skytemple-files.git
 pip3 uninstall -y skytemple-icons && pip3 install git+https://github.com/SkyTemple/skytemple-icons.git

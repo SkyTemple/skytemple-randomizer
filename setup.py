556ea834b2d43451143306684266be5d5fd0b473
Capypara <hello@capypara.de>
2022-10-31 13:17:47 +0100

    Python 3.11 CI

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
         ],
         package_data={'skytemple_randomizer': package_data},
         entry_points='''

346b3324b18c03cbc47a60fcf63414ac84cdafb6
Capypara <hello@capypara.de>
2024-03-31 22:55:24 +0200

    Basic dungeon randomization

--- skytemple_randomizer/frontend/gtk/widgets/page_dungeons_chances.py
+++ skytemple_randomizer/frontend/gtk/widgets/page_dungeons_chances.py
@@ -17,6 +17,7 @@
 from __future__ import annotations
 
 import os
+from typing import cast
 
 from skytemple_randomizer.config import RandomizerConfig
 from skytemple_randomizer.frontend.gtk.path import MAIN_PATH
@@ -27,6 +28,81 @@ from gi.repository import Gtk, Adw
 @Gtk.Template(filename=os.path.join(MAIN_PATH, "page_dungeons_chances.ui"))
 class DungeonsChancesPage(Adw.PreferencesPage):
     __gtype_name__ = "StDungeonsChancesPage"
+    row_random_weather_chance = cast(Adw.SpinRow, Gtk.Template.Child())
+    row_max_mh_chance = cast(Adw.SpinRow, Gtk.Template.Child())
+    row_max_hs_chance = cast(Adw.SpinRow, Gtk.Template.Child())
+    row_max_ks_chance = cast(Adw.SpinRow, Gtk.Template.Child())
+    row_max_sticky_chance = cast(Adw.SpinRow, Gtk.Template.Child())
+
+    randomization_settings: RandomizerConfig | None
+    _suppress_signals: bool
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.randomization_settings = None
+        self._suppress_signals = False
 
     def populate_settings(self, config: RandomizerConfig):
-        pass
+        self._suppress_signals = True
+        self.randomization_settings = config
+
+        self.row_random_weather_chance.set_value(
+            config["dungeons"]["random_weather_chance"].value
+        )
+        self.row_max_mh_chance.set_value(config["dungeons"]["max_mh_chance"].value)
+        self.row_max_hs_chance.set_value(config["dungeons"]["max_hs_chance"].value)
+        self.row_max_ks_chance.set_value(config["dungeons"]["max_ks_chance"].value)
+        self.row_max_sticky_chance.set_value(
+            config["dungeons"]["max_sticky_chance"].value
+        )
+
+        self._suppress_signals = False
+
+    @Gtk.Template.Callback()
+    def on_row_random_weather_chance_notify_value(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["random_weather_chance"].value = int(
+            self.row_random_weather_chance.get_value()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_max_mh_chance_notify_value(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["max_mh_chance"].value = int(
+            self.row_max_mh_chance.get_value()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_max_hs_chance_notify_value(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["max_hs_chance"].value = int(
+            self.row_max_hs_chance.get_value()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_max_ks_chance_notify_value(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["max_ks_chance"].value = int(
+            self.row_max_ks_chance.get_value()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_max_sticky_chance_notify_value(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["max_sticky_chance"].value = int(
+            self.row_max_sticky_chance.get_value()
+        )
--- skytemple_randomizer/frontend/gtk/widgets/page_dungeons_settings.py
+++ skytemple_randomizer/frontend/gtk/widgets/page_dungeons_settings.py
@@ -17,6 +17,7 @@
 from __future__ import annotations
 
 import os
+from typing import cast
 
 from skytemple_randomizer.config import RandomizerConfig
 from skytemple_randomizer.frontend.gtk.path import MAIN_PATH
@@ -27,6 +28,110 @@ from gi.repository import Gtk, Adw
 @Gtk.Template(filename=os.path.join(MAIN_PATH, "page_dungeons_settings.ui"))
 class DungeonsSettingsPage(Adw.PreferencesPage):
     __gtype_name__ = "StDungeonsSettingsPage"
+    row_layouts = cast(Adw.SwitchRow, Gtk.Template.Child())
+    row_weather = cast(Adw.SwitchRow, Gtk.Template.Child())
+    row_items = cast(Adw.SwitchRow, Gtk.Template.Child())
+    row_monsters = cast(Adw.SwitchRow, Gtk.Template.Child())
+    row_traps = cast(Adw.SwitchRow, Gtk.Template.Child())
+    row_fixed_rooms = cast(Adw.SwitchRow, Gtk.Template.Child())
+    row_min_floor_change_percent = cast(Adw.SpinRow, Gtk.Template.Child())
+    row_max_floor_change_percent = cast(Adw.SpinRow, Gtk.Template.Child())
+
+    randomization_settings: RandomizerConfig | None
+    _suppress_signals: bool
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.randomization_settings = None
+        self._suppress_signals = False
 
     def populate_settings(self, config: RandomizerConfig):
-        pass
+        self._suppress_signals = True
+        self.randomization_settings = config
+
+        self.row_layouts.set_active(config["dungeons"]["layouts"])
+        self.row_weather.set_active(config["dungeons"]["weather"])
+        self.row_items.set_active(config["dungeons"]["items"])
+        self.row_monsters.set_active(config["dungeons"]["pokemon"])
+        self.row_traps.set_active(config["dungeons"]["traps"])
+        self.row_fixed_rooms.set_active(config["dungeons"]["fixed_rooms"])
+        self.row_min_floor_change_percent.set_value(
+            config["dungeons"]["min_floor_change_percent"].value
+        )
+        self.row_max_floor_change_percent.set_value(
+            config["dungeons"]["max_floor_change_percent"].value
+        )
+
+        self._suppress_signals = False
+
+    @Gtk.Template.Callback()
+    def on_row_layouts_notify_active(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["layouts"] = (
+            self.row_layouts.get_active()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_weather_notify_active(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["weather"] = (
+            self.row_weather.get_active()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_items_notify_active(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["items"] = self.row_items.get_active()
+
+    @Gtk.Template.Callback()
+    def on_row_monsters_notify_active(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["pokemon"] = (
+            self.row_monsters.get_active()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_traps_notify_active(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["traps"] = self.row_traps.get_active()
+
+    @Gtk.Template.Callback()
+    def on_row_fixed_rooms_notify_active(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["fixed_rooms"] = (
+            self.row_fixed_rooms.get_active()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_min_floor_change_percent_notify_value(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["min_floor_change_percent"].value = int(
+            self.row_min_floor_change_percent.get_value()
+        )
+
+    @Gtk.Template.Callback()
+    def on_row_max_floor_change_percent_notify_value(self, *args):
+        if self._suppress_signals:
+            return
+        assert self.randomization_settings is not None
+        self.randomization_settings["dungeons"]["max_floor_change_percent"].value = int(
+            self.row_max_floor_change_percent.get_value()
+        )
